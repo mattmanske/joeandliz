fae30866e5ac76e0532e09db339cc2507692714a
polymatt <matt@polymathic.me>
2013-08-06 20:56:06 -0500

    Site updated to c6c3154

---
+++ js/app-aeafe9a6.js
@@ -0,0 +1,1 @@
+!function(){$(function(){return $("#bridalparty > a").click(function(){return $("#bridalparty").attr("data-showing",1)}),$("#bridalparty .headshot-icons li").click(function(){return $("#bridalparty").attr("data-showing",$(this).data("slide"))})})}.call(this);
