111bbe04668dbafe63b5f4d7ddc4dcc662e0309f
polymatt <matt@polymathic.me>
2013-08-09 17:53:13 -0500

    Site updated to 58ae0b3

--- js/app-aeafe9a6.js
+++
@@ -1,1 +0,0 @@
-!function(){$(function(){return $("#bridalparty > a").click(function(){return $("#bridalparty").attr("data-showing",1)}),$("#bridalparty .headshot-icons li").click(function(){return $("#bridalparty").attr("data-showing",$(this).data("slide"))})})}.call(this);
